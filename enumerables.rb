03c493bc5cf1c1b1669392e59c5b75e08a473cce
Bonnie Li <bonnieli@Bonnies-MacBook-Pro.local>
2022-09-26 16:31:31 -0400

    reached flatten

--- enumerables.rb
+++ enumerables.rb
@@ -1,3 +1,4 @@
+require "byebug"
 class Array
     def my_each (&prc)
         i = 0
@@ -17,7 +18,53 @@ class Array
         end
         finalArray
     end
+
+    def my_reject(&prc)
+        i = 0
+        finalArray = []
+        while i < self.length
+            finalArray << self[i] if !prc.call(self[i])
+            i+=1
+        end
+        finalArray
+    end
+
+    def my_any?(&prc)
+        i = 0 
+        while i < self.length
+            return true if prc.call(self[i])
+            i +=1
+        end
+        false
+    end
+
+    def my_all?(&prc)
+        i = 0
+        counter = 0
+        while i < self.length
+            counter += 1 if prc.call(self[i])
+            i +=1
+        end
+        return true if counter == self.length
+        false
+    end
+
+    def my_flatten 
+        #return  if 
+        result = []
+        self.each do |ele| # ele = array or num or string
+            debugger
+            if ele.is_a?(Array)
+                ele.my_flatten 
+            else
+                result << ele
+            end
+        end
+        result
+    end
 end
+
+p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten
 
 arr = [1,2,3,4]
 block1 = Proc.new {|ele| ele*2}
@@ -25,3 +72,11 @@ prc2 = Proc.new {|ele| ele.even?}
 
 p arr.my_each{|ele| ele*2}
 p arr.my_select{|ele| ele.even?}
+p arr.my_reject{|ele| ele.even?}
+
+a = [1, 2, 3]
+p a.my_any? { |num| num > 1 } # => true
+p a.my_any? { |num| num == 4 } # => false
+p a.my_all? { |num| num > 1 } # => false
+p a.my_all? { |num| num < 4 } # => true
+
