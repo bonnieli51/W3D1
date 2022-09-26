26ebd761692a44f7156f645cddaf24d95b50f1fa
Gleb Mirzayev <Your Email>
2022-09-26 16:11:08 -0400

    added my_select

--- enumerables.rb
+++ enumerables.rb
@@ -7,4 +7,21 @@ class Array
         end
         self
     end
+
+    def my_select(&prc)
+        i = 0
+        finalArray = []
+        while i < self.length
+            finalArray << self[i] if prc.call(self[i])
+            i+=1
+        end
+        finalArray
+    end
 end
+
+arr = [1,2,3,4]
+block1 = Proc.new {|ele| ele*2}
+prc2 = Proc.new {|ele| ele.even?}
+
+p arr.my_each{|ele| ele*2}
+p arr.my_select{|ele| ele.even?}
